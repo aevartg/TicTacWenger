2a3f2a4d2289f384e32bd411a32f08b6e3829dd6
aevartg <aevartg@gmail.com>
2016-10-24 11:21:59 +0000

    unit test setup and main class created

---
+++ src/main/java/TicTacToe.java
@@ -0,0 +1,7 @@
+public class TicTacToe {
+	public static void main(String[] args){		
+	}
+	public static int testTester(){
+		return 0;
+	}
+}
---
+++ src/test/java/TicTacToeTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TicTacToeTest {
+    @Test 
+    public void testSomeLibraryMethod() {
+        assertEquals(0, TicTacToe.testTester());
+    }
+}
