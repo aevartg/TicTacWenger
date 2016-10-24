f3752bb16f03862ba3006b06753533173f684f3f
aevartg <aevartg@gmail.com>
2016-10-24 12:23:25 +0000

    Travis branch prufa

--- src/test/java/TicTacToeTest.java
+++ src/test/java/TicTacToeTest.java
@@ -4,6 +4,6 @@ import static org.junit.Assert.*;
 public class TicTacToeTest {
     @Test 
     public void testSomeLibraryMethod() {
-        assertEquals(0, TicTacToe.testTester());
+        assertEquals(1, TicTacToe.testTester());
     }
 }
