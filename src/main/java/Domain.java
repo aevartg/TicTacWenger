b6994fd142cceaaf3a2fd90923e710a38706c39d
Magnús Gunnarsson <maknoos@i-1217c9a8.cs1004cloud.internal>
2016-10-26 12:36:22 +0000

    added the main function call

---
+++ src/main/java/Domain.java
@@ -0,0 +1,6 @@
+public class Domain{
+	public static void playGame()
+	{
+		char activePlayer = 'x';
+	}
+}
--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,5 +1,7 @@
 public class TicTacToe {
-	public static void main(String[] args){		
+	public static void main(String[] args){
+		Domain gameDomain = new Domain();
+		gameDomain.playGame();
 	}
 	public static int testTester(){
 		return 1;
