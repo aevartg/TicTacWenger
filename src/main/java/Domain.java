f8bcad5a8e95ff5c0ad7312430479ffd87221c73
Magnús Gunnarsson <maknoos@i-1217c9a8.cs1004cloud.internal>
2016-10-27 11:31:32 +0000

    a little bit of progress on the playgame function

--- src/main/java/Domain.java
+++ src/main/java/Domain.java
@@ -2,11 +2,17 @@ public class Domain{
 	public static void playGame()
 	{
 		char activePlayer = 'x';
+		char [][] gameBoard =  new char [3] [3];
 		boolean gameOver = false;
 		while(!gameOver)
 		{
+
 			//function calls
 		}
 
+	}
+
+	private static void initializeBoard(char gameBoard){
+		
 	}
 }
