dd6a35940161debf5c9d586746b39fbbb15a395f
olafurv15 <olafurv15@ru.is>
2016-10-27 22:53:18 +0000

    getInput refactored with exception cases: valid number

--- src/main/java/Interface.java
+++ src/main/java/Interface.java
@@ -1,12 +1,12 @@
 import java.io.*;
 
 public class Interface {
-	public static int getInput(char[][] arr, String player) throws IOException
+	public static int getInput(char[][] arr, char player) throws IOException
 	{
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		boolean valid = false;
 		
-		System.out.print(player + ", choose a number between 1 - 9: ");
+		System.out.print("Player " + player + ", choose a number between 1 - 9: ");
 		
 		int result = 0;
 		
@@ -16,11 +16,27 @@ public class Interface {
 			{
 				result = Integer.parseInt(in.readLine());
 				valid = true;
+
+				if(result <= 9 && result >= 1) 
+				{
+					result = result - 1;
+					if(arr[result/3][result%3] == 'X' || arr[result/3][result%3] == 'O')
+					{
+						System.out.print("This board position is taken! Choose another number between 1 - 9: ");
+					}
+					else
+					{
+						valid = true;
+					}
+				}
+				else 
+				{
+					System.out.print("The number has to be between 1 - 9! Choose another number between 1 - 9: ");
+				}
 			}
 			catch (NumberFormatException e)
 			{
-					System.out.println("Input error - Input has to be a number!");
-					System.out.print("Choose a number between 1 - 9: ");
+				System.out.print("Input was not a integer. Choose a number between 1 - 9: ");
 			}
 		}while(!valid);
 		
