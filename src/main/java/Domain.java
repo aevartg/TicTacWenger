dc1970ad8d9681abd2b2b4c7fae277809fc92798
aevartg <aevartg@gmail.com>
2016-10-28 10:31:42 +0000

    fix indentation

--- src/main/java/Domain.java
+++ src/main/java/Domain.java
@@ -35,49 +35,49 @@ public class Domain{
 		return gameBoard;
 	}
 	public static char checkStatus(char gameBoard[][])
-        {
-	        for(int i = 0; i < BOARDSIZE; i++)
-	        {
-	          if(gameBoard[i][0] == gameBoard[i][1] && gameBoard[i][0] == gameBoard[i][2])
-	          {
-	          return gameBoard[i][0];
-	          }
-	          if(gameBoard[0][i] == gameBoard[1][i] && gameBoard[0][i] == gameBoard[2][i])
-	          {
-	          return gameBoard[0][i];
-	          }
+  {
+    for(int i = 0; i < BOARDSIZE; i++)
+    {
+      if(gameBoard[i][0] == gameBoard[i][1] && gameBoard[i][0] == gameBoard[i][2])
+      {
+      return gameBoard[i][0];
+      }
+      if(gameBoard[0][i] == gameBoard[1][i] && gameBoard[0][i] == gameBoard[2][i])
+      {
+      return gameBoard[0][i];
+      }
 
-	        }
+    }
 
-	        if(gameBoard[0][0] ==  gameBoard[1][1] && gameBoard[2][2] == gameBoard[0][0] )
-	        {
-	                return gameBoard[0][0];
-	        }
-	        else if(gameBoard[0][2] == gameBoard[1][1] && gameBoard[2][0] == gameBoard[0][2] )
-	        {
-	                return gameBoard[0][2];
-	        }
-	        else
-	        {
-	        				return 'D';
-	        }
-        }
+    if(gameBoard[0][0] ==  gameBoard[1][1] && gameBoard[2][2] == gameBoard[0][0] )
+    {
+            return gameBoard[0][0];
+    }
+    else if(gameBoard[0][2] == gameBoard[1][1] && gameBoard[2][0] == gameBoard[0][2] )
+    {
+            return gameBoard[0][2];
+    }
+    else
+    {
+    				return 'D';
+    }
+  }
 
-        public static char[][] updateBoard(char[][] arr, char player, int number)
-        {
-          arr[number/3][number%3] = player;
-          return arr;
-        }
+  public static char[][] updateBoard(char[][] arr, char player, int number)
+  {
+    arr[number/3][number%3] = player;
+    return arr;
+  }
 
-				public static char switchPlayer(char player)
-				{
-					if(player == 'X')
-					{
-						return 'O';
-					}
-					else
-					{
-						return 'X';
-					}
-				}
+	public static char switchPlayer(char player)
+	{
+		if(player == 'X')
+		{
+			return 'O';
+		}
+		else
+		{
+			return 'X';
+		}
+	}
 }
