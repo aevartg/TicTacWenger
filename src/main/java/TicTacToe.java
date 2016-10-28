e54f83e42b10ade68ef766aebdafa4616691f1b1
Magnus Gunnarsson <magnusgun15@ru.is>
2016-10-28 09:12:12 +0000

    fix for Travis

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,5 +1,7 @@
+import java.io.*;
 public class TicTacToe {
-	public static void main(String[] args){
+	public static void main(String[] args) throws IOException
+	{
 		Domain gameDomain = new Domain();
 		gameDomain.playGame();
 	}
