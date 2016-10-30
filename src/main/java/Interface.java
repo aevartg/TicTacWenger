6a8d9ba02d1c41bc46adde240a7236293245754a
olafurv15 <olafurv15@ru.is>
2016-10-30 19:53:38 +0000

    getInput refactored

--- src/main/java/Interface.java
+++ src/main/java/Interface.java
@@ -9,44 +9,39 @@ public class Interface
 	public static int getInput(char[][] arr, char player) throws IOException
 	{
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		boolean valid = false;
 		
 		System.out.print("Player " + player + ", choose a number between 1 - 9: ");
 		
 		int result = 0;
 		
-		do
+
+		try
 		{
-			try
+			// Throws exception if input is not a integer
+			result = Integer.parseInt(in.readLine());
+
+			if(result <= 9 && result >= 1) 
 			{
-				// Throws exception if input is not a integer
-				result = Integer.parseInt(in.readLine());
-				valid = true;
+				result = result - 1;
 
-				if(result <= 9 && result >= 1) 
+				// Checks whether input position is valid or not
+				if(arr[result/3][result%3] == 'X' || arr[result/3][result%3] == 'O')
 				{
-					result = result - 1;
-
-					// Checks whether input position is valid or not
-					if(arr[result/3][result%3] == 'X' || arr[result/3][result%3] == 'O')
-					{
-						System.out.print("This board position is taken! Choose another number between 1 - 9: ");
-					}
-					else
-					{
-						valid = true;
-					}
-				}
-				else 
-				{
-					System.out.print("The number has to be between 1 - 9! Choose another number between 1 - 9: ");
+					System.out.println("This board position is taken!");
+					return getInput(arr, player);
 				}
 			}
-			catch (NumberFormatException e)
+			else 
 			{
-				System.out.print("Input was not a integer. Choose a number between 1 - 9: ");
+				System.out.println("The number has to be between 1 - 9!");
+				return getInput(arr, player);
 			}
-		}while(!valid);
+		}
+		catch (NumberFormatException e)
+		{
+			System.out.println("Input was not a integer.");
+			return getInput(arr, player);
+		}
 		
 		return result;
 	}
